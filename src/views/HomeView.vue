52a781732225675ce8c3f5da54254420cacc53dd
Bertil Vossebelt <github@ajvossebelt.nl>
2025-03-17 02:35:17 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <div class="small-display-box">
+      <h1 class="logo">&lt;Oog/&gt;</h1>
+      <p class="description">The ultra flexible and simple application logging solution.</p>
+      <a href="/signup" class="a-register">Get started</a>
+      <a href="/login" class="a-login">Or login here</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.description {
+  font-size: 20px;
+  margin: 10px;
+  text-align: center;
+}
+
+.a-register {
+  margin: 10px 0 10px 0;
+  padding: calc(5px + 0.1vw) calc(30px + 10vw);
+  box-sizing: border-box;
+  background-color: #fff;
+  font-weight: bold;
+  font-size: 2rem;
+  border: none;
+  border-radius: 180px;
+  color: #000;
+  text-decoration: none;
+  transition: 0.2s;
+}
+
+.a-register:hover {
+  outline: 2px solid #fff;
+  outline-offset: 5px;
+  transition: 0.2s ease-out;
+}
+
+.a-register:focus {
+  outline: 2px solid #fff;
+  outline-offset: 5px;
+  transition: 0.2s ease-out;
+}
+
+
+.a-login {
+  color: #fff;
+  padding: 10px;
+  font-size: 1.1rem;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+interface Credentials {
+  username: string
+  password: string
+}
+
+const credentials: Credentials = reactive({
+  username: "",
+  password: "",
+})
+
+const register = async () => {
+  fetch("https://localhost:4040/api/v1/account/authenticate", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+    credentials: "include"
+  })
+    .then((response) => {
+      if (!response.ok) throw Error(`Failed to login: ${response.statusText}`)
+      console.log("Login successful:", response.json())
+    })
+    .catch((error) => {
+      console.error("An Error occurred during login:", error)
+    })
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="small-display-box">
+      <h1 class="logo">&lt;LogIn/&gt;</h1>
+      <form @submit.prevent class="align-contents-center">
+        <!-- E-mail address -->
+        <label for="UsernameInput">E-mail address:</label>
+        <input id="UsernameInput" type="text" v-model="credentials.username" />
+
+        <!-- Password -->
+        <label for="PasswordInput">Password:</label>
+        <input id="PasswordInput" type="password" v-model="credentials.password" />
+      </form>
+    </div>
+
+    <button type="submit" class="submit-btn" @click="register">LogIn</button>
+    <RouterLink to="/signup" class="register-link">Don't have an account?</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  width: 95%;
+}
+
+label {
+  width: 80%;
+  align-self: flex-start;
+  display: block;
+  margin-top: 5px;
+  color: #fff;
+  font-style: italic;
+}
+
+input {
+  display: block;
+  width: 100%;
+  margin: 10px 0;
+  background-color: #404040;
+  padding: 15px;
+  border-radius: 10px;
+  border: none;
+  color: #fff;
+  font-size: 1.2rem;
+}
+
+input:focus {
+  outline: 2px solid #404040;
+  outline-offset: 5px;
+  transition: 0.2s ease-out;
+}
+
+.small-display-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: calc(18rem + 30vw);
+  max-width: 90%;
+  padding: 20px;
+  border-radius: 10px;
+  border: 1px solid #404040;
+  margin-bottom: 20px;
+}
+
+.submit-btn {
+  padding: 15px;
+  font-size: 1.2rem;
+  border-radius: 10px;
+  border: none;
+  background-color: #404040;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+  width: calc(18rem + 30vw);
+}
+
+.submit-btn:hover {
+  outline: 2px solid #404040;
+  outline-offset: 5px;
+  transition: 0.2s ease-out;
+}
+
+.submit-btn:focus {
+  outline: 2px solid #404040;
+  outline-offset: 5px;
+  transition: 0.2s ease-out;
+}
+
+.register-link {
+  color: #fff;
+  padding: 10px;
+  font-size: 1.1rem;
+}
+</style>
